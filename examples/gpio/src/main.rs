1a53e2f18c4ce7fa6e5623db5365a17fa636579d
Jean Fontena <jean.silvafontena@nxp.com>
2023-05-30 10:08:27 +0200

    Zero-sized structures for GPIOs

--- examples/gpio/src/main.rs
+++ examples/gpio/src/main.rs
@@ -1,8 +1,7 @@
 #![allow(dead_code)]
 
-struct GpioConfig<ENABLED, DIRECTION> {
-    port: u32,    
-    gpio: u32,    
+struct GpioConfig<ENABLED, DIRECTION, PIN> {
+    pin: PIN,
     enabled: ENABLED,
     direction: DIRECTION,
 }
@@ -16,47 +15,56 @@ struct Output;
 struct Input;
 struct DontCare;
 
+// PINS
+#[derive(Debug)]
+struct PORTA_0;
+
+#[derive(Debug)]
+struct PORTA_1;
+
+#[derive(Debug)]
+struct PORTB_0;
+
+#[derive(Debug)]
+struct PORTB_1;
+
 // functions available for all pins
-impl<ENABLED, DIRECTION> GpioConfig<ENABLED, DIRECTION> {
-    pub fn new(port: u32, gpio: u32) -> GpioConfig<Disabled, DontCare> {
+impl<ENABLED, DIRECTION, PIN> GpioConfig<ENABLED, DIRECTION, PIN> {
+    pub fn new(pin: PIN) -> GpioConfig<Disabled, DontCare, PIN> {
         // call you hardware api here to mark the pin as disabled/high-z/whatever
 
         GpioConfig {
-            port,
-            gpio,
+            pin,
             enabled: Disabled,
             direction: DontCare,
         }        
     }
 
-    pub fn into_enabled_output(self) -> GpioConfig<Enabled, Output> {
+    pub fn into_enabled_output(self) -> GpioConfig<Enabled, Output, PIN> {
         // call your hardware api to set the pin as output
 
         GpioConfig {
-            port: self.port,
-            gpio: self.gpio,
+            pin: self.pin,
             enabled: Enabled,
             direction: Output,
         }
     }
 
-    pub fn into_enabled_input(self) -> GpioConfig<Enabled, Input> {
+    pub fn into_enabled_input(self) -> GpioConfig<Enabled, Input, PIN> {
         // call your hardware api to set the pin as input
 
         GpioConfig {
-            port: self.port,
-            gpio: self.gpio,
+            pin: self.pin,
             enabled: Enabled,
             direction: Input,
         }
     }
 
-    pub fn disable(self) -> GpioConfig<Disabled, DontCare> {
+    pub fn disable(self) -> GpioConfig<Disabled, DontCare, PIN> {
         // call your hardware api to disable the pin
 
         GpioConfig {
-            port: self.port,
-            gpio: self.gpio,
+            pin: self.pin,
             enabled: Disabled,
             direction: DontCare,
         }
@@ -64,41 +72,41 @@ impl<ENABLED, DIRECTION> GpioConfig<ENABLED, DIRECTION> {
 }
 
 // only enabled for input pins
-impl GpioConfig<Enabled, Input> {
+impl<PIN: std::fmt::Debug> GpioConfig<Enabled, Input, PIN> {
     pub fn read(&self) -> bool {
         // call your hardware api to read the pin
+        println!("Reading pin {:?}.", self.pin);
         true
     }
 }
 
 // only enabled for output pins
-impl GpioConfig<Enabled, Output> {
+impl<PIN: std::fmt::Debug> GpioConfig<Enabled, Output, PIN> {
     pub fn set_high(&self) {
         // call your hardware api to set the pin to high
-        println!("Setting pin {:x}:{} to high.", self.port, self.gpio)
+        println!("Setting pin {:?} to high.", self.pin)
     }
 
     pub fn set_low(&self) {
         // call your hardware api to set the pin to low
-        println!("Setting pin {:x}:{} to high.", self.port, self.gpio)
+        println!("Setting pin {:?} to low.", self.pin)
     }
 }
 
 // problems?
-//    - port and gpio can be swapped when calling new
+//    - port and gpio can be swapped when calling new --> SOLVED
 //    - we can have two structs setting up the same pin
-//    - each GPIO takes 8 bytes
+//    - each GPIO takes 8 bytes ---> SOLVED
 
 fn main() {
-    let pin = GpioConfig::<Disabled, DontCare>::new(0xFEFABBBB, 0);
+    let pin = GpioConfig::<Disabled, DontCare, _>::new(PORTA_0);
     let input_pin = pin.into_enabled_input();
     println!("read pin: {}", input_pin.read());
 
-    let pin = GpioConfig::<Disabled, DontCare>::new(0xFEFABBBB, 1);
+    let pin = GpioConfig::<Disabled, DontCare, _>::new(PORTB_0);
     let output_pin = pin.into_enabled_output();
     output_pin.set_high();
 
-    println!("sizeof disabled pin: {:?}", std::mem::size_of::<GpioConfig::<Disabled, DontCare>>());
-    println!("sizeof disabled pin: {:?}", std::mem::size_of::<GpioConfig::<Enabled, Output>>());
-    println!("sizeof disabled pin: {:?}", std::mem::size_of::<GpioConfig::<Enabled, Input>>());
+    println!("sizeof input pin: {:?}", std::mem::size_of_val(&input_pin));
+    println!("sizeof output pin: {:?}", std::mem::size_of_val(&output_pin));
 }
